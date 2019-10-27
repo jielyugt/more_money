03a06dbff0b99142a04b094b1d9dcbdde3d88c40
jielyugt <jie.lyu@gatech.edu>
2019-10-26 21:33:53 -0400

    Login UI improved

--- app/src/main/java/uk/ivanc/archi/login.java
+++ app/src/main/java/uk/ivanc/archi/login.java
@@ -1,94 +1,47 @@
 package uk.ivanc.archi;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.app.Activity;
 import android.graphics.Color;
-import android.os.Bundle;
-import android.view.View;
-
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.adapter.rxjava.HttpException;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import uk.ivanc.archi.model.GithubService;
-import uk.ivanc.archi.model.Item;
-import uk.ivanc.archi.model.ItemService;
-import uk.ivanc.archi.model.Repository;
 
 public class login extends Activity {
 
-    private static final String TAG = "MainActivity";
-
-    private Subscription subscription;
-    private RecyclerView reposRecycleView;
-    private Toolbar toolbar;
-    private EditText editTextUsername;
-    private ProgressBar progressBar;
-    private TextView infoTextView;
-    private ImageButton searchButton;
-    Button b1, b2;
-    EditText ed1, ed2;
+    Button login;
+    EditText username, password;
     TextView tx1;
     int counter = 3;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
-        ItemService itemService = ItemService.Factory.create();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginin_page);
-        b1 = (Button) findViewById(R.id.button);
-        ed1 = (EditText) findViewById(R.id.editText);
-        ed2 = (EditText) findViewById(R.id.editText2);
-
-        b2 = (Button) findViewById(R.id.button);
-        tx1 = (TextView) findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);
+        setContentView(R.layout.login);
+        login = (Button) findViewById(R.id.login_button);
+        username = (EditText) findViewById(R.id.username_input);
+        password = (EditText) findViewById(R.id.password_input);
 
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ed1.getText().toString().equals("admin") &&
-                        ed2.getText().toString().equals("admin")) {
+                if (username.getText().toString().equals("admin") &&
+                        password.getText().toString().equals("admin")) {
                     Toast.makeText(getApplicationContext(),
                             "Redirecting...", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent("user.activity_main");
                     startActivity(intent);
                 } else {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-
-                    tx1.setVisibility(View.VISIBLE);
-                    tx1.setBackgroundColor(Color.RED);
-                    counter--;
-                    tx1.setText(Integer.toString(counter));
-
                     if (counter == 0) {
-                        b1.setEnabled(false);
+                        login.setEnabled(false);
+                        Toast.makeText(getApplicationContext(), "Account locked after 3 many failed attempts", Toast.LENGTH_SHORT).show();
+                    } else {
+                        counter--;
+                        Toast.makeText(getApplicationContext(), "Wrong credentials, " + counter + " attempts remaining", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
